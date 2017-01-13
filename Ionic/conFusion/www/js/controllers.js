7ca91d97f39b27c621329715d25ea43f548f77fd
Giuseppe <giuse.scara@gmail.com>
2017-01-13 10:04:23 +0100

    Ionic Modals, Forms, Popovers - Assignment 2

--- Ionic/conFusion/www/js/controllers.js
+++ Ionic/conFusion/www/js/controllers.js
@@ -154,23 +154,61 @@ angular.module('conFusion.controllers', [])
   };
 }])
 
-.controller('DishDetailController', ['$scope', '$stateParams', 'menuFactory', 'baseURL', function($scope, $stateParams, menuFactory, baseURL) {
+.controller('DishDetailController', ['$scope', '$stateParams', 'menuFactory', 'baseURL','$ionicPopover','favoriteFactory','$ionicModal',function($scope, $stateParams, menuFactory, baseURL, $ionicPopover, favoriteFactory, $ionicModal) {
   $scope.baseURL = baseURL;
   $scope.dish = {};
   $scope.showDish = false;
   $scope.message="Loading ...";
+  $scope.mycomment = {rating:5, comment:"", author:"", date:""};
 
   $scope.dish = menuFactory.getDishes().get({id:parseInt($stateParams.id,10)})
   .$promise.then(
-                  function(response){
-                      $scope.dish = response;
-                      $scope.showDish = true;
-                  },
-                  function(response) {
-                      $scope.message = "Error: "+response.status + " " + response.statusText;
-                  }
+    function(response){
+        $scope.dish = response;
+        $scope.showDish = true;
+    },
+    function(response) {
+        $scope.message = "Error: "+response.status + " " + response.statusText;
+    }
   );
 
+    $ionicModal.fromTemplateUrl('templates/dish-comment.html', {
+      scope: $scope,
+      animation: 'slide-in-up'
+    }).then(function(modal) {
+      $scope.modal = modal;
+    });
+
+    $ionicPopover.fromTemplateUrl('templates/dish-detail-popover.html', {
+       scope: $scope
+     }).then(function(popover) {
+       $scope.popover = popover;
+     });
+
+  $scope.openPopover = function($event) {
+    $scope.popover.show($event);
+  };
+
+  $scope.addToFavorites = function() {
+    console.log(favoriteFactory);
+    favoriteFactory.addToFavorites($scope.dish.id);
+    $scope.popover.hide();
+  };
+
+  $scope.addComment = function() {
+    $scope.modal.show();
+    $scope.popover.hide();
+  };
+
+  $scope.submitComment = function () {
+
+      $scope.mycomment.date = new Date().toISOString();
+
+      $scope.dish.comments.push($scope.mycomment);
+      menuFactory.getDishes().update({id:$scope.dish.id},$scope.dish);
+      $scope.mycomment = {rating:5, comment:"", author:"", date:""};
+      $scope.modal.hide();
+  }
 
 }])
 
